241ab88370d5a4c5e03f344e38a05db355d4bb9f
RobinP789 <122293369+RobinP789@users.noreply.github.com>
2026-03-03 17:16:25 -0500

    Update NPC.py

--- NPC.py
+++ NPC.py
@@ -1,13 +1,13 @@
 from random import randint
 import random
 
-age = ['young', 'middle age', 'elderly']
+age = ['young', 'young', 'young', 'middle age', 'middle age', 'elderly']
 location = ['an isolated area', 'a town', 'a city']
 gender = ['male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'male', 'female', 'nonbinary individual', 'nonbinary individual']
-marriage = ['married', 'not married']
-kids = ['one child', 'two children', 'three children', 'four children', 'five children', 'six or more children']
-vibe = ['girly pop', 'scholarly', 'vexing', 'n intense', 'relaxed', 'n artistic', 'type A', 'pious', 'sychophant', 'jock', 'goth', 'n inquisitive', 'generous']
-quirk = ['narcolepsy', 'RBF', 'many piercings', 'a large scar', 'monochomatic clothing']
+marriage = ['married', 'married', 'not married', 'not married', 'not married']
+kids = ['no children', 'no children', 'no children', 'one child', 'one child', 'two children', 'two children', 'two children', 'three or more children']
+vibe = ['girly pop', 'scholarly', 'horny', 'vexing', 'n intense', 'relaxed', 'n artistic', 'type A', 'pious', 'sychophant', 'jock', 'goth', 'n inquisitive', 'generous']
+quirk = ['narcolepsy', 'RBF', 'many piercings', 'a large scar', 'monochomatic clothing', 'a colourful scarf', 'a missing finger', 'tattoos', 'an elaborate hairstyle', 'heterochromia', 'a pet cat', 'a pet dog', 'a pet bird', 'a pet reptile', 'a gambling addiction', 'a drinking problem', 'the mouth of a sailor', 'a large mole', 'a pock-marked face', 'glasses', 'glasses', 'glasses', 'a lazy eye', 'disheveled hair', 'a visible birthmark', 'a shellfish allergy', 'a nut allergy', 'a bee allergy', 'lactose intolerance', 'bad breath', 'exessive body hair', 'a limp', 'a loud laugh', 'a hyena-like laugh']
 
 rage = random.choice(age)
 rloc = random.choice(location)
@@ -24,3 +24,4 @@ print("who is", rmar)
 
 if rmar == 'married':
         print("and has", rkid)
+
